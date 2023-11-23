df147fc4108e3ad4c8355a6968e8ddda266b99ac
Sasha Xox <sasha_xox@tut.by>
2023-11-23 17:13:19 +0300

    Header

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,19 @@ import SignIn from "./pages/signin";
 import Profile from "./pages/profile";
 import About from "./pages/about";
 import SignUp from "./pages/signup";
+import Header from "./components/header";
 
 export default function App() {
-  return <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home/>}/> 
-      <Route path="/sign-in" element={<SignIn/>}/> 
-      <Route path="/sign-up" element={<SignUp/>}/> 
-      <Route path="/profile" element={<Profile/>}/> 
-      <Route path="/about" element={<About/>}/> 
-    </Routes>
-  </BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
