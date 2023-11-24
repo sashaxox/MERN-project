621afd8b6b8306f0661f9a2565329a646a98f75d
Sasha Xox <sasha_xox@tut.by>
2023-11-24 20:19:32 +0300

    signup

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// только с впн
+mongoose.connect(process.env.MONGODB).then (()=> {
+  console.log("Connected");
+}).catch((err)=>{
+  console.log(err);
+})
+
 
 const app = express();
 
