ad4720f4a2ae0b1fd9e5210d941f80b40ae01682
Sasha Xox <sasha_xox@tut.by>
2023-11-25 18:21:12 +0300

    sign in function

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../modals/user.model.js";
 import bcryptjs from "bcryptjs";
-import { errorHandler } from '../utils/error.js';
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -9,6 +10,25 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json("User created successfully!");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found"));
+    const vaildPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!vaildPassword)
+      return next(errorHandler(401, "Invalid Password or Email"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
